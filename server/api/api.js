c66e1efeb3e3024a07a455f03613f02761b8e3ea
Cristian Casa <xthecapx@gmail.com>
2016-03-21 11:45:34 -0500

    Hello world React working, node server api rest on file done

--- server/api/api.js
+++ server/api/api.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var fs = require('fs');
 
 var DATA_DIR = path.join(__dirname, '..', 'data');
+var FILE = "";
 
-router.get('*', function(req, res) {
-  res.sendfile(path.join(DATA_DIR, req.url));
+router.get('/:file', function(req, res) {
+  FILE = DATA_DIR + "/" + req.params.file + ".json";
+  res.sendFile(FILE);
+});
+
+router.post('*', function(req, res) {
+  var configFile = fs.readFileSync(FILE);
+  var config = JSON.parse(configFile);
+  config.push(req.body);
+  var configJSON = JSON.stringify(config);
+  fs.writeFileSync(FILE, configJSON);
 });
 
 module.exports = router;
--- server/server/server.js
+++ server/server/server.js
@@ -1,16 +1,14 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var logger = require('morgan');
 var path = require('path');
 var api = require('./../api/api.js');
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // this is used for parsing the JSON object from POST
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, '../../public')));
-app.use('./../api/api.js', api);
-app.use(function(req, res, next) {
-     res.header("Access-Control-Allow-Origin", "*");
-     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-     next();
-   });
+app.use('/api', api);
 
 module.exports = app;
